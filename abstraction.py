d77cb2fcf0eeb6e4bb6fd7a64d4f9f569827f6e1
Tobi <=>
2020-12-08 10:17:39 -0600

    Minor changes made to abstraction

--- abstraction.py
+++ abstraction.py
@@ -1,33 +1,40 @@
 import pymysql
 
-class Nokubase:
-    def __init__(self, settings={}):
 
-        requirements = [
-            "host",
-            "user",
-            "password",
-            "db"
-        ]
+class Nokubase:
+    def __init__(self, settings: dict = {}):
+
+        requirements: list = ["host", "user", "password", "db"]
 
         for requirement in requirements:
             if not settings.get(requirement):
-                raise Exception("You're missing a requirement: " + requirement)
+                raise Exception(f"You're missing a requirement: {requirement}")
 
-        settings["charset"] = settings.get("charset") if settings.get("charset") else "utf8mb4"
+        settings["charset"] = (
+            settings.get("charset") if settings.get("charset") else "utf8mb4"
+        )
 
-        settings["cursorclass"] = settings.get("cursorclass") if settings.get("cursorclass") else pymysql.cursors.DictCursor
+        settings["cursorclass"] = (
+            settings.get("cursorclass")
+            if settings.get("cursorclass")
+            else pymysql.cursors.DictCursor
+        )
 
         self.connection = pymysql.connect(**settings)
 
-    def isEmailTaken(self, email):
+    def isEmailTaken(self, email: str) -> bool:
         with self.connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT email FROM registered WHERE email = %s",
-                (
-                    email,
-                )
-            )
+            cursor.execute("SELECT email FROM account WHERE email = %s", (email,))
+            result = cursor.fetchone()
+            print(result)
+            if result:
+                return True
+            else:
+                return False
+
+    def isNickTaken(self, nick: str) -> bool:
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT nick FROM account WHERE nick = %s", (nick,))
             result = cursor.fetchone()
             if result:
                 return True
